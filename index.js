1a98ddbd85e3c366a69d2850a3cfdc7d57f5f5d6
stephen <stephen@farver.io>
2018-08-25 13:48:31 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your code in this file!
+
+function scuberGreetingForFeet(distance) {
+  let response
+  if (distance < 400) {
+    response = 'This one is on me!'
+  } else if (distance > 2500) {
+    response = 'No can do.'
+  } else if (distance > 2000) {
+    response = 'I will gladly take your thirty bucks.'
+  }
+
+  return response
+}
+
+function ternaryCheckCity(city) {
+  return city == 'NYC' ? 'Ok, sounds good.' : "No go."
+}
+
+function switchOnCharmFromTip(tipAmount) {
+  switch (tipAmount) {
+    case 'generous':
+      return 'Thank you so much.'
+      break
+    case 'not as generous':
+      return 'Thank you.'
+      break
+    default:
+      return 'Bye.'
+  }
+}
