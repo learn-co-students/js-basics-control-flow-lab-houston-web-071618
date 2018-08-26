b10d8d045a30cd66d80b8270d5f062ffdcf144a8
Flatiron School <flatironschool@Flatirons-MacBook-Air-3.local>
2018-08-25 23:17:00 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Write your code in this file!
+function scuberGreetingForFeet(arg) {
+  if(arg<=400)
+  {
+    return "This one is on me!";
+  }  //freesample;
+  else if(arg>=2000 && arg <= 2500)
+  {
+    // charge 30
+    return 'I will gladly take your thirty bucks.';
+  }
+  else
+  {
+    return 'No can do.';
+  }
+}
+
+function ternaryCheckCity(city){
+  if(city === 'NYC')
+  {
+    return "Ok, sounds good."
+  }else {
+    return "No go."
+  }
+}
+
+function switchOnCharmFromTip(tip){
+
+  switch(tip){
+    case "generous":
+      return "Thank you so much."
+
+    case "not as generous":
+      return "Thank you."
+
+    default:
+      return "Bye."
+
+  }
+  
+}
